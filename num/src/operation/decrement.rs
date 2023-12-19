4469d4d01c22d827efdee80e0d0d31189f049b9e
or-n <net.educator@gmail.com>
2023-12-19 12:50:25 +0100

    remove comment in increment and decrement

--- num/src/operation/decrement.rs
+++ num/src/operation/decrement.rs
@@ -10,18 +10,3 @@ impl<T: One + Sub<Output = T>> Decrement for T {
         self - Self::one()
     }
 }
-/*
-
-macro_rules! impl_for {
-    ($($t:ty),*) => {$(
-        impl Decrement for $t {
-            #[inline]
-            fn decrement(self) -> $t {
-                self - <$t>::one()
-            }
-        }
-    )*};
-}
-
-impl_for!(usize, isize, u8, u16, u32, u64, i8, i16, i32, i64, f32, f64);
-*/
