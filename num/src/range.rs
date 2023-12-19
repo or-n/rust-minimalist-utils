79fe15237731de8850a8dde2af58bfb546bafe82
or-n <net.educator@gmail.com>
2023-12-19 12:41:15 +0100

    remove comment in range

--- num/src/range.rs
+++ num/src/range.rs
@@ -46,27 +46,3 @@ where
         other.mix(self.ratio(mix))
     }
 }
-
-/*
-impl<T: Copy> Range<T> {
-    pub fn mix(&self, ratio: f32) -> T
-    where
-        T: Mul<f32, Output = T> + Add<Output = T>,
-    {
-        self.start * (1.0 - ratio) + self.end * ratio
-    }
-
-    pub fn ratio(&self, mix: T) -> f32
-    where
-        T: Sub<Output = T> + Div<Output = f32>,
-    {
-        (mix - self.start) / (self.end - self.start)
-    }
-
-    pub fn map<'a>(&'a self, other: &'a Self) -> impl Fn(T) -> T + 'a
-    where
-        T: Mul<f32, Output = T> + Add<Output = T> + Sub<Output = T> + Div<Output = f32>,
-    {
-        move |mix| other.mix(self.ratio(mix))
-    }
-}*/
