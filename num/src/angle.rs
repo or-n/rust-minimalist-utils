7ae9d1b95c7cd5ae436f1a01349878640b0203cb
or-n <net.educator@gmail.com>
2023-12-19 12:21:19 +0100

    rename Angle trait to ToAngle, FromAngle and ToAngle impl for f64

--- num/src/angle.rs
+++ num/src/angle.rs
@@ -1,19 +1,27 @@
-pub trait Angle {
-    fn angle(x: Self, y: Self) -> Self;
+pub trait FromAngle {
+    fn from_angle(self) -> (Self, Self)
+    where
+        Self: Sized;
 }
 
-impl Angle for f32 {
-    fn angle(x: Self, y: Self) -> Self {
-        f32::atan2(x, y)
-    }
+pub trait ToAngle {
+    fn to_angle(x: Self, y: Self) -> Self;
 }
 
-pub trait FromAngle where Self: Sized {
-    fn from_angle(self) -> (Self, Self);
+macro_rules! impl_for {
+    ($($t:ty),*) => { $(
+        impl FromAngle for $t {
+            fn from_angle(self) -> (Self, Self) {
+                (self.cos(), self.sin())
+            }
+        }
+
+        impl ToAngle for $t {
+            fn to_angle(x: Self, y: Self) -> Self {
+                Self::atan2(y, x)
+            }
+        }
+    )*};
 }
 
-impl FromAngle for f32 {
-    fn from_angle(self) -> (Self, Self) {
-        self.sin_cos()
-    }
-}
+impl_for!(f32, f64);
