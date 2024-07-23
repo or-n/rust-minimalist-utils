3e310900feff5bc8bec161e0146c5591173eb9ad
or-n <kulombowie@gmail.com>
2024-07-23 07:38:12 +0200

    minmax changes

--- game/src/minmax.rs
+++ game/src/minmax.rs
@@ -2,21 +2,27 @@ use crate::eval::*;
 use crate::update::*;
 use num::range::*;
 
+pub trait Invert {
+    fn invert(&self) -> Self;
+}
+
 macro_rules! min_max {
     (   $my:ident, $enemy:ident,
         $better:ident, $better_than: tt,
         $my_f:ident, $enemy_f:ident
     ) => {
-        pub fn $my_f<State, Move, MoveError, Score, R>(
+        pub fn $my_f<State, Player, Move, UpdateError, Score>(
             depth: usize,
+            player: Player,
             mut pruning: Range<Score>,
             state: State,
-        ) -> Result<(Option<Move>, Score), MoveError>
+        ) -> Result<(Option<Move>, Score), UpdateError>
         where
             State: Copy + AvailableMoves<Move>
-                + Update<(), Move, MoveError, R> + Eval<Score>,
+                + Update<(Player, Move), UpdateError> + Eval<Score>,
             Move: Copy,
             Score: Ord + Copy,
+            Player: Copy + Invert,
         {
             if depth == 0 {
                 return Ok((None, state.score()));
@@ -32,15 +38,16 @@ macro_rules! min_max {
             match moves.next() {
                 Some(mut best_move) => {
                     let mut best_score =
-                        updated(state, (), best_move)?.score();
+                        state.updated((player, best_move))?.score();
                     if prune(best_score, &mut pruning) {
                         return Ok((Some(best_move), best_score));
                     }
                     for this_move in moves {
                         let (_, score) = $enemy_f(
                             depth - 1,
+                            player.invert(),
                             pruning,
-                            updated(state, (), this_move)?,
+                            state.updated((player, this_move))?,
                         )?;
                         if score $better_than best_score {
                             best_score = score;
--- game/src/update.rs
+++ game/src/update.rs
@@ -2,26 +2,27 @@ pub trait AvailableMoves<Move> {
     fn available_moves(&self) -> Vec<Move>;
 }
 
-pub trait Update<Player, Move, MoveError, R>
+pub trait Update<Move, Error>
 where
     Self: Sized,
 {
-    fn update(
-        &mut self,
-        player: Player,
-        m: Move,
-    ) -> Result<R, MoveError>;
+    fn update(&mut self, m: Move) -> Option<Error>;
 }
 
-pub fn updated<State, Player, Move, MoveError, R>(
-    state: State,
-    player: Player,
-    m: Move,
-) -> Result<State, MoveError>
+pub trait Updated<Move, Error>
 where
-    State: Clone + Update<Player, Move, MoveError, R>,
+    Self: Sized,
 {
-    let mut new = state.clone();
-    let _ = new.update(player, m)?;
-    Ok(new)
+    fn updated(&self, m: Move) -> Result<Self, Error>;
+}
+
+impl<State, Move, Error> Updated<Move, Error> for State
+where
+    State: Clone + Update<Move, Error>,
+{
+    fn updated(&self, m: Move) -> Result<Self, Error> {
+        let mut new = self.clone();
+        new.update(m).map_or(Ok(()), Err)?;
+        Ok(new)
+    }
 }
