0aaeffcddfa2ab49db1dcdcd2bcd88fcf32f4bc9
or-n <kulombowie@gmail.com>
2024-11-26 11:03:28 +0100

    now generic: dependency_db key-value eat

--- dependency_db/src/read.rs
+++ dependency_db/src/read.rs
@@ -1,20 +1,27 @@
 use eat::*;
+use std::hash::Hash;
 
 pub struct KeyValue<Hash>((Hash, (Vec<u8>, Vec<Hash>)));
 
-impl Eat<&[u8], (), ()> for KeyValue<u32> {
-    fn eat(i: &[u8], _data: ()) -> Result<(&[u8], Self), ()> {
-        let (i, key) = u32::eat(i, ())?;
+impl<'a, T> Eat<&'a [u8], (), ()> for KeyValue<T>
+where
+    T: Eat<&'a [u8], (), ()>,
+{
+    fn eat(i: &'a [u8], _data: ()) -> Result<(&'a [u8], Self), ()> {
+        let (i, key) = T::eat(i, ())?;
         let (i, file) = SeqN::<u8>::eat(i, ())?;
-        let (i, deps) = SeqN::<u32>::eat(i, ())?;
+        let (i, deps) = SeqN::<T>::eat(i, ())?;
         let value = (file.0, deps.0);
         Ok((i, KeyValue((key, value))))
     }
 }
 
-impl Eat<&[u8], (), ()> for super::Deps<u32> {
-    fn eat(i: &[u8], _data: ()) -> Result<(&[u8], Self), ()> {
-        let (i, deps) = KeyValue::<u32>::eat_many(i, ());
+impl<'a, T> Eat<&'a [u8], (), ()> for super::Deps<T>
+where
+    T: Eq + Hash + Eat<&'a [u8], (), ()>,
+{
+    fn eat(i: &'a [u8], _data: ()) -> Result<(&'a [u8], Self), ()> {
+        let (i, deps) = KeyValue::<T>::eat_many(i, ());
         let table = deps.into_iter().map(|x| x.0).collect();
         Ok((i, super::Deps { table }))
     }
