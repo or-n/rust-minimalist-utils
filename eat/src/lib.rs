65b38f991bc54ef042cdccc87ad07facf74297e7
or-n <kulombowie@gmail.com>
2024-11-26 10:56:18 +0100

    fix: u32 and SeqN from &[u8] in eat

--- eat/src/lib.rs
+++ eat/src/lib.rs
@@ -60,3 +60,32 @@ where
         (i, count)
     }
 }
+
+impl Eat<&[u8], (), ()> for u32 {
+    fn eat(i: &[u8], _data: ()) -> Result<(&[u8], Self), ()> {
+        let (i, b0) = u8::eat(i, ())?;
+        let (i, b1) = u8::eat(i, ())?;
+        let (i, b2) = u8::eat(i, ())?;
+        let (i, b3) = u8::eat(i, ())?;
+        let x = u32::from_be_bytes([b0, b1, b2, b3]);
+        Ok((i, x))
+    }
+}
+
+pub struct SeqN<T>(pub Vec<T>);
+
+impl<'a, T> Eat<&'a [u8], (), ()> for SeqN<T>
+where
+    T: Eat<&'a [u8], (), ()>,
+{
+    fn eat(i: &'a [u8], _data: ()) -> Result<(&'a [u8], Self), ()> {
+        let (mut i, n) = u32::eat(i, ())?;
+        let mut r = Vec::new();
+        for _ in 0..n {
+            let (new_i, x) = T::eat(i, ())?;
+            i = new_i;
+            r.push(x);
+        }
+        Ok((i, SeqN(r)))
+    }
+}
