33224ee58c21af9c7eabaa1581c8efd105458333
or-n <kulombowie@gmail.com>
2024-12-17 14:26:51 +0100

    remove eat HashMap in dependency_db

--- dependency_db/src/lib.rs
+++ dependency_db/src/lib.rs
@@ -1,23 +1,23 @@
 pub mod dep;
-pub mod read;
 
 #[cfg(test)]
 mod tests {
     use super::dep::{DepValue, DEPENDENCIES};
-    use super::read::Table;
     use eat::*;
+    use std::collections::HashMap;
     use std::fs;
 
     #[test]
     fn test_1_dep() {
         let bytes = fs::read(DEPENDENCIES).unwrap();
-        let (i, deps) = Table::<u32, DepValue<u32>>::eat(&bytes[..], ()).unwrap();
+        let (i, id_value) = <(u32, DepValue<u32>)>::eat_many(&bytes[..], ());
+        let deps: HashMap<_, _> = id_value.into_iter().collect();
         assert!(i.is_empty());
-        assert!(deps.table.len() == 1);
+        assert!(deps.len() == 1);
         let value = DepValue {
             bytes: vec![],
             deps: vec![],
         };
-        assert_eq!(deps.table.get_key_value(&2137), Some((&2137, &value)));
+        assert_eq!(deps.get_key_value(&2137), Some((&2137, &value)));
     }
 }
