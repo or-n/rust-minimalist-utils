47c00b9af6bf0f92817d1c43b7eceb1299a83cfd
or-n <kulombowie@gmail.com>
2024-12-17 12:55:07 +0100

    to_seq no &mut

--- to_seq/src/lib.rs
+++ to_seq/src/lib.rs
@@ -2,22 +2,21 @@ pub trait ToSeq<Output, Error> {
     fn to_seq(self, o: Output) -> Result<Output, Error>;
 }
 
-impl ToSeq<&mut Vec<u8>, ()> for &[u8] {
-    fn to_seq(self, o: &mut Vec<u8>) -> Result<&mut Vec<u8>, ()> {
+impl ToSeq<Vec<u8>, ()> for &[u8] {
+    fn to_seq(self, mut o: Vec<u8>) -> Result<Vec<u8>, ()> {
         o.extend_from_slice(self);
         Ok(o)
     }
 }
 
-impl ToSeq<&mut Vec<u8>, ()> for u32 {
-    fn to_seq(self, o: &mut Vec<u8>) -> Result<&mut Vec<u8>, ()> {
-        self.to_be_bytes().to_seq(o)?;
-        Ok(o)
+impl ToSeq<Vec<u8>, ()> for u32 {
+    fn to_seq(self, o: Vec<u8>) -> Result<Vec<u8>, ()> {
+        self.to_be_bytes().to_seq(o)
     }
 }
 
-impl ToSeq<&mut Vec<u8>, ()> for &Vec<u8> {
-    fn to_seq(self, o: &mut Vec<u8>) -> Result<&mut Vec<u8>, ()> {
+impl ToSeq<Vec<u8>, ()> for &Vec<u8> {
+    fn to_seq(self, mut o: Vec<u8>) -> Result<Vec<u8>, ()> {
         o.extend(self);
         Ok(o)
     }
