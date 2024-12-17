2597f01fe78ce87f720a92ba72cb8c06e0c68e28
or-n <kulombowie@gmail.com>
2024-12-17 12:36:32 +0100

    to_seq no type data parameter

--- dependency_db/src/write.rs
+++ dependency_db/src/write.rs
@@ -1,15 +1,15 @@
 use super::Table;
 use to_seq::*;
 
-impl<'a, Id, Value> ToSeq<&'a mut Vec<u8>, (), ()> for Table<Id, Value>
+impl<'a, Id, Value> ToSeq<&'a mut Vec<u8>, ()> for Table<Id, Value>
 where
-    Id: ToSeq<&'a mut Vec<u8>, (), ()>,
-    Value: ToSeq<&'a mut Vec<u8>, (), ()>,
+    Id: ToSeq<&'a mut Vec<u8>, ()>,
+    Value: ToSeq<&'a mut Vec<u8>, ()>,
 {
-    fn to_seq(self, mut o: &'a mut Vec<u8>, _data: ()) -> Result<&mut Vec<u8>, ()> {
+    fn to_seq(self, mut o: &'a mut Vec<u8>) -> Result<&mut Vec<u8>, ()> {
         for (id, value) in self.table {
-            o = id.to_seq(o, ())?;
-            o = value.to_seq(o, ())?;
+            o = id.to_seq(o)?;
+            o = value.to_seq(o)?;
         }
         Ok(o)
     }
--- to_seq/src/lib.rs
+++ to_seq/src/lib.rs
@@ -1,22 +1,22 @@
-pub trait ToSeq<Output, Data, Error> {
+pub trait ToSeq<Output, Error> {
     fn to_seq(self, o: Output) -> Result<Output, Error>;
 }
 
-impl ToSeq<&mut Vec<u8>, (), ()> for &[u8] {
+impl ToSeq<&mut Vec<u8>, ()> for &[u8] {
     fn to_seq(self, o: &mut Vec<u8>) -> Result<&mut Vec<u8>, ()> {
         o.extend_from_slice(self);
         Ok(o)
     }
 }
 
-impl ToSeq<&mut Vec<u8>, (), ()> for u32 {
+impl ToSeq<&mut Vec<u8>, ()> for u32 {
     fn to_seq(self, o: &mut Vec<u8>) -> Result<&mut Vec<u8>, ()> {
         self.to_be_bytes().to_seq(o)?;
         Ok(o)
     }
 }
 
-impl ToSeq<&mut Vec<u8>, (), ()> for &Vec<u8> {
+impl ToSeq<&mut Vec<u8>, ()> for &Vec<u8> {
     fn to_seq(self, o: &mut Vec<u8>) -> Result<&mut Vec<u8>, ()> {
         o.extend(self);
         Ok(o)
