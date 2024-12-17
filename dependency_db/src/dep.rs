7c9c68fa458017d42d3665e9cdb6cbff5ed99cdc
or-n <kulombowie@gmail.com>
2024-12-17 12:38:25 +0100

    update to_seq in dependency_db

--- dependency_db/src/dep.rs
+++ dependency_db/src/dep.rs
@@ -24,16 +24,16 @@ where
     }
 }
 
-impl<'a, Id> ToSeq<&'a mut Vec<u8>, (), ()> for DepValue<Id>
+impl<'a, Id> ToSeq<&'a mut Vec<u8>, ()> for DepValue<Id>
 where
-    Id: ToSeq<&'a mut Vec<u8>, (), ()>,
+    Id: ToSeq<&'a mut Vec<u8>, ()>,
 {
-    fn to_seq(self, mut o: &'a mut Vec<u8>, _data: ()) -> Result<&'a mut Vec<u8>, ()> {
-        o = (self.bytes.len() as u32).to_seq(o, ())?;
-        o = self.bytes.to_seq(o, ())?;
-        o = (self.deps.len() as u32).to_seq(o, ())?;
+    fn to_seq(self, mut o: &'a mut Vec<u8>) -> Result<&'a mut Vec<u8>, ()> {
+        o = (self.bytes.len() as u32).to_seq(o)?;
+        o = self.bytes.to_seq(o)?;
+        o = (self.deps.len() as u32).to_seq(o)?;
         for dep in self.deps {
-            o = dep.to_seq(o, ())?;
+            o = dep.to_seq(o)?;
         }
         Ok(o)
     }
